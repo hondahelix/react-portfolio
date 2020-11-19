14bbba157a9cf0bcb9763ceec1366bc172f628fb
Jonathan Honda <hondahelix120@gmail.com>
2020-11-18 20:08:32 -0800

    react starter code

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Nav from "./components/Navbar";
+// import Home from "./components/Home";
+//import WorkPage from ""
+     //import Resume from "./components/Resume";
+//import Footer from "";
+
+function App() {
+  return (
+    <p>hello world</p>
+  );
+}
+
+export default App;
+
+{/* <Router>
+<div>
+  <Nav />
+  <br/>
+  <Switch>
+
+    <Route exact path="/" component={Home} />
+    <Route exact path="/resume" src= {process.env.PUBLIC_URL + '/Honda-Resume.pdf'}/>
+  </Switch>
+  <br/>
+</div>
+</Router> */}

aa1610ecb85618e4cee56ac36912321d428e74a0
Jonathan Honda <hondahelix120@gmail.com>
2020-12-28 17:37:11 -0800

    cleaned up code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,34 +5,31 @@ function Home (){
     return (
         <div className = "container">
             <div id = "aboutMe">
-            <div className="center-align">
-            <br></br> 
-                <img id = "profile" src={profile} className= "responsive-img circle" alt="Me"/>
+                <div className="center-align">
+                    <br></br> 
+                    <img id = "profile" src={profile} className= "responsive-img circle" alt="Me"/>
                 </div>
                 <div>
                     <h3 className="center-align" >Jonathan Honda</h3>
                     <p>
                         My name is Jonathan Honda, and I am A FullStack web developer. I have completed a coding Bootcamp from the University of California, Berkeley. 
-                        I graduated with an overall grade of an A.
+                        I graduated with an overall grade of an A. 
                     </p>
                     <p>
                         I am originally from San Diego, California, and I have graduated this year from the University of California, Irvine, with a BA in Criminology Law and Society.
-                         Go Anteaters Zot Zot Zot!!! However, I now live in Northern California. 
+                        Go Anteaters Zot Zot Zot!!! However, I now live in Northern California. 
                     </p>
                     <p>
                         I am actively searching for work as a web developer, and I look forward to honing and strengthen my coding skills.
                     </p>
                     <p>
                         Another exciting thing about me is that I was in the ROTC program all four years in college and have commissioned as a second lieutenant in the Army National Guard.
-                         On the weekends, I serve as a Network Officer at the Division Signal Company at Joint Forces Training Base in Los Alamitos. 
+                        On the weekends, I serve as a Network Officer at the Division Signal Company at Joint Forces Training Base in Los Alamitos. 
                     </p>
+                    <p><a id = "email" className="center-align" href="mailto: hondahelix120@gmail.com">Email: hondahelix120@gmail.com</a></p>
                 </div>
-                <b></b>
-                <a id = "email" href="mailto: hondahelix120@gmail.com">Email: hondahelix120@gmail.com</a>
-            <br></br>
-
+                <br></br>
             </div>
-
         </div>
     )
 

7f1293bebce7871b15a994c11648d1394436e364
Jonathan Honda <hondahelix120@gmail.com>
2021-02-03 17:56:10 -0800

    problem with gh pages trying to re push it

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ import "./nav.css"
 function Navbar (){
     return(
         <nav className="nav-center" role="navigation">
-            <div className="nav-wrapper ">
+            <div className="nav-wrapper">
             <ul id="nav-mobile" className="navbar">
                 <li ><Link to = {(process.env.PUBLIC_URL || "/")+"/"}><i className = "material-icons">home</i></Link></li>
                 <li ><Link to = {(process.env.PUBLIC_URL || "/")+ "/work"}> <i className = "material-icons">folder_open</i></Link></li>
